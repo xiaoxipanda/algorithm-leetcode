493e7826c1df4c02e3bbf126a1cde346c1e3ebda
markingwang <markingwang@tencent.com>
2021-05-12 21:49:57 +0800

    no015

---
+++ src/main/java/com/code/no015/Solution.java
@@ -0,0 +1,20 @@
+package com.code.no015;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/3sum/
+ */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+
+
+
+        return result;
+    }
+}
